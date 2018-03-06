79ef38d8ff0700ba7334315d5b592a35a9f28d87
Felipe Martínez <pipernene@gmail.com>
2018-03-06 16:09:17 +0100

    Set up mod class

---
+++ ArduinoMod/ArduinoMod.cs
@@ -0,0 +1,21 @@
+﻿using PiTung.Console;
+using PiTung;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ArduinoMod
+{
+    public class ArduinoMod : Mod
+    {
+        public override string Name => "Arduino";
+        public override string PackageName => "me.pipe01.ArduinoMod";
+        public override string Author => "pipe01";
+        public override Version ModVersion => new Version("1.0.0");
+
+        public override void BeforePatch()
+        {
+
+        }
+    }
+}
