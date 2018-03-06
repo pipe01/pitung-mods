791bfc06b72284851b05cc4372a2686fc29f66ff
Felipe Martínez <pipernene@gmail.com>
2018-03-06 15:56:51 +0100

    Create ArduinoMod

---
+++ ArduinoMod/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoMod
+{
+    public class Class1
+    {
+    }
+}
