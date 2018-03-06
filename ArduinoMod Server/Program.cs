990d38f1075be1bb971ee5f541ab9bff8834923a
Felipe Martínez <pipernene@gmail.com>
2018-03-06 16:23:50 +0100

    Added server

--- ArduinoMod Server/Program.cs
+++ ArduinoMod Server/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Threading;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +9,24 @@ namespace ArduinoMod_Server
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) => new Program().Main();
+
+        private Server Server;
+        private ManualResetEventSlim ExitEvent = new ManualResetEventSlim();
+
+        public void Main()
         {
+            Console.WriteLine("Starting server...");
+
+            Server = new Server();
+            Server.DebugLog += (_, e) => Console.WriteLine("-> " + e);
+            Server.Start();
+
+            Console.WriteLine("Listening");
+
+            ExitEvent.Wait();
+
+            Server.Stop(false);
         }
     }
 }
---
+++ ArduinoMod Server/Server.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoMod_Server
+{
+    internal class Server
+    {
+        private TcpListener Listener;
+        private TcpClient Remote;
+
+        private const int ServerPort = 5757;
+
+        public event EventHandler<string> Log = delegate { };
+        public event EventHandler<string> DebugLog = delegate { };
+
+        public Server()
+        {
+            Listener = new TcpListener(IPAddress.Loopback, ServerPort);
+        }
+
+        public void Start()
+        {
+            Listener.Start();
+
+            AcceptClient();
+        }
+
+        private void AcceptClient()
+        {
+            DebugLog(this, "Accepting clients...");
+
+            Listener.BeginAcceptTcpClient(AcceptTcpClient, null);
+
+            void AcceptTcpClient(IAsyncResult ar)
+            {
+                DebugLog(this, "Client found.");
+
+                Remote = Listener.EndAcceptTcpClient(ar);
+
+                DebugLog(this, "Connected to client.");
+
+                Send("hi");
+            }
+        }
+
+        public void Stop(bool acceptNewClient)
+        {
+            if (Remote == null)
+                return;
+
+            DebugLog(this, "Stopping.");
+
+            Remote.Close();
+            Remote = null;
+
+            if (acceptNewClient)
+                AcceptClient();
+        }
+        
+        private void Send(string str)
+        {
+            if (Remote == null)
+                return;
+
+            byte[] bytes = Encoding.UTF8.GetBytes(str);
+            var stream = Remote.GetStream();
+
+            stream.Write(bytes, 0, bytes.Length);
+            stream.Flush();
+        }
+    }
+}
