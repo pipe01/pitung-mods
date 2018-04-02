a53228dfe4d78339d3d33ff70b046e6965d14e5d
Felipe Martínez <pipernene@gmail.com>
2018-04-02 20:16:19 +0200

    Added ThroughInverter mod

---
+++ ThroughInverter/MyInverter.cs
@@ -0,0 +1,36 @@
+﻿using PiTung.Components;
+using References;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace ThroughInverter
+{
+    public class MyInverter : UpdateHandler
+    {
+        public static void Register()
+        {
+            var b = PrefabBuilder
+                .Custom(() =>
+                {
+                    var obj = GameObject.Instantiate(Prefabs.ThroughBlotter);
+                    GameObject.Destroy(obj.GetComponent<Blotter>());
+
+                    var plate = obj.transform.Find("BottomPlate").gameObject;
+                    plate.GetComponent<MegaMeshComponent>().MaterialType = MaterialType.CircuitBoard;
+                    plate.GetComponent<Renderer>().material.color = new Color(0.6f, 1, 1);
+
+                    return obj;
+                });
+
+            ComponentRegistry.CreateNew<MyInverter>(MyMod.Instance, "throughinverter", "Through Inverter", b);
+        }
+
+        protected override void CircuitLogicUpdate()
+        {
+            Outputs[0].On = !Inputs[0].On;
+        }
+    }
+}
